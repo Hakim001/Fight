a102caab711a00975e123b6c2e406ed2b627defe
Your Name <you@example.com>
2017-12-27 16:23:49 +0100

    rajout de l'auth dans logout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,8 +25,31 @@
     </ul>
     <div class="form-group">
        <div class="nav navbar-nav">
-           <li> <a href="{{ route('login')}}">login</a></li>
-           <li> <a href=" {{ route('register')}}">register</a></li>
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endguest
+           
         </div>    
     </div>
     
